d0fc97eb2d742e7178c634fa9db1df2c999007db
Ross Timson <ross@rosstimson.com>
2014-02-14 16:26:51 +0000

    Version bump to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email  'ross@rosstimson.com'
 license           'Apache 2.0'
 description       'Installs dtach'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.1.0'
+version           '1.0.0'
 
 %w{ debian ubuntu centos amazon }.each do |os|
   supports os
